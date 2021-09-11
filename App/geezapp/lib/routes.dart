daa474a27028d40867b9bce05540caabb7836ae9
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-09-11 02:17:42 -0700

    Update routes.dart

--- App/geezapp/lib/routes.dart
+++ App/geezapp/lib/routes.dart
@@ -22,7 +22,7 @@ import 'login/login_auth.dart';
 import 'login/repository/login.dart';
 
 class AppRoute {
-  static const initialRoute = UserHomePage.routeName;
+  static const initialRoute = WelcomeScreen.routeName;
   static final routes = {
     WelcomeScreen.routeName: (BuildContext context) => WelcomeScreen(),
     LoginScreen.routeName: (BuildContext context) => LoginScreen(
